c7ea5398e5409d7cb7d8a61511d418a98a2f1696
Slizh96 <slizh.olya@mail.ru>
2022-08-31 17:11:38 +0300

    create some components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://assets-global.website-files.com/60abb689ce5c94972a5f808a/6107298ce4b5511f5f772f3f_Telos%20Logo%20Small.png"
+                alt=""/>
+        </header>
+    )
+}
---
+++ src/components/Profile.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://sites.google.com/site/prirodanaha/_/rsrc/1468741750018/home/%D0%96%D0%B8%D0%B2%D0%B0%D1%8F-%D0%BF%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0.jpg" alt=""/>
+            </div>
+            <div>
+                <img src="https://i.doska.by/images/2022-01-14/6273/UX8OHklg/animals-dogs-newfoundland-1-1.800.jpg" alt="" className='logo'/>
+                <span>I'm beautiful dog. <br/> My name is Nju. <br/> I live in minsk. <br/>I'm 10 y.o.</span>
+            </div>
+            <div>
+                <h3>My posts</h3>
+            </div>
+            <div>post 1</div>
+            <div>post 2</div>
+            <div>post 3</div>
+            <div>post 4</div>
+            <div>post 5</div>
+        </div>
+    )
+}
