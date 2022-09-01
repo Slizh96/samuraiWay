dacc1683113760a56d9ea4c41b18ce981d815ba1
Slizh96 <slizh.olya@mail.ru>
2022-09-01 18:58:33 +0300

    18 create dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header/Header";
 import {Nav} from "./components/Nav/Nav";
-import {Profile} from "./components/Profile/Profile";
+// import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
 
 function App() {
     return (
         <div className="appWrapper">
             <Header/>
             <Nav/>
-            <Profile/>
+            <div className='appWrapperContent'>
+            {/*<Profile/>*/}
+            <Dialogs/>
+            </div>
         </div>
     );
 }
