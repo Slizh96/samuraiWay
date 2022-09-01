fc0cdb8a098ebbb8f172955696bd16191135f4e1
Slizh96 <slizh.olya@mail.ru>
2022-09-01 22:53:50 +0300

    create dialogs component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,34 @@ import s from './Dialogs.module.css'
 export const Dialogs = () => {
     return (
         <div className={s.dialogs}>
-            lol
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Olya
+                </div>
+                <div className={s.dialog}>
+                    Sergey
+                </div>
+                <div className={s.dialog}>
+                    Valentina
+                </div>
+                <div className={s.dialog}>
+                    Alex
+                </div>
+                <div className={s.dialog}>
+                    Max
+                </div>
+                <div className={s.dialog}>
+                    Tim
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hello</div>
+                <div className={s.message}>How are you</div>
+                <div className={s.message}>What's new?</div>
+                <div className={s.message}>I'm fine</div>
+                <div className={s.message}>I'm lucky</div>
+                <div className={s.message}>Great</div>
+            </div>
         </div>
     )
 }
