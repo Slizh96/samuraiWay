b7917e9b534fbda2640d0fba1024f6ef9c5aac18
Slizh96 <slizh.olya@mail.ru>
2022-09-01 18:25:10 +0300

    16 props myPosts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -7,13 +7,14 @@ export const MyPosts = () => {
             <h3>My posts</h3>
 
             <div>
-                New post
+                <textarea value='Write new post'/>
+                <div><button>Add</button></div>
             </div>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
-            <Post/>
+            <Post post='Hello. This is my first post' like={25}/>
+            <Post post='How are you?' like={15}/>
+            <Post post='Great. Thanks, and you?' like={38}/>
+            <Post post='I am lucky' like={51}/>
+            <Post post='Good luck' like={85}/>
         </div>
 
     )
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import s from './Post.module.css'
 
-export const Post = () => {
+type PostPropsType={
+    post: string
+    like:number
+}
+
+export const Post = (props: PostPropsType) => {
     return (
         <div>
 
             <div className={s.posts}>
                 <div className={s.item}>
                     <img src="https://lapkins.ru/upload/iblock/4a8/4a86cff4ab81d72fe1dea71242323a8c.jpg" alt=""/>
-                    post 1</div>
-
+                    {props.post}
+                </div>
+                <div>
+                    <span>like: {props.like}</span>
+                </div>
             </div>
         </div>
     )
