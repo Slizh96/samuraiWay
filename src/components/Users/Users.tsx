8987548055ebe8a60aacdb6d400dd26bb216346c
Slizh96 <slizh.olya@mail.ru>
2022-11-02 00:49:46 +0300

    users class component. constructor

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -10,34 +10,35 @@ import avatar from '../../assets/images/avatar.jpg'
 //     setUsers:(users:usersType[])=>void
 // }
 
-class Users extends React.Component<any, any>{
+class Users extends React.Component<any, any> {
 
-    getUsers=()=>{
-        if (this.props.users.length===0){
+    constructor(props: any) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(response => {
+                // debugger;
+                this.props.setUsers(response.data.items)
+            });
+    }
 
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
-                .then(response=>{
-                    // debugger;
-                    this.props.setUsers(response.data.items)
-                });
 
-
-        }
-    };
-
-    render(){
+    render() {
         return (
             <div className={s.users}>
-                <button onClick={this.getUsers}>Get Users</button>
-                {this.props.users.map((u: { id: React.Key | null | undefined; photos: { small: string | null | undefined; }; followed: any; name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; status: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) => <div key={u.id}>
+                {this.props.users.map((u: { id: React.Key | null | undefined; photos: { small: string | null | undefined; }; followed: any; name: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; status: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) =>
+                    <div key={u.id}>
                     <span>
                         <div>
                             <img src={u.photos.small != null ? u.photos.small : avatar} alt='avatar'/>
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={()=>{this.props.unfollow(u.id)}}>Unfollow</button>
-                                : <button onClick={()=>{this.props.follow(u.id)}}>Follow</button>}
+                                ? <button onClick={() => {
+                                    this.props.unfollow(u.id)
+                                }}>Unfollow</button>
+                                : <button onClick={() => {
+                                    this.props.follow(u.id)
+                                }}>Follow</button>}
                         </div>
                     </span>
                         <span>
